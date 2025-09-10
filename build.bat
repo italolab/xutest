6777ffbf278662683caacfaff3e3219d977e88e9
Ítalo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2025-09-10 13:17:48 -0300

    chore: Criando Script Batch de Empacotamento em .zip

---
+++ build.bat
@@ -0,0 +1,71 @@
+
+@echo off
+
+set VERSION=1.0
+set SO=windows
+set ARCH=x64
+
+set APP_FILE_NAME=cbtest
+set LIB_FILE_NAME=libcbtest.a
+
+set BUILD_FOLDER=build
+set APP_FOLDER=cbtest-%VERSION%-%SO%-%ARCH%
+
+set APP_DIR=%BUILD_FOLDER%\%APP_FOLDER%
+
+set APP_LIB_FILE=%APP_DIR%\%LIB_FILE_NAME%
+
+rem LIMPANDO...
+
+if exist %APP_DIR% (
+    rmdir /s /q %APP_DIR%
+    echo Removido: %APP_DIR%
+)
+
+if exist %APP_DIR%.zip (
+    del %APP_DIR%.zip
+    echo Removido: %APP_DIR%.zip
+)
+
+mkdir %APP_DIR%
+
+rem COMPILANDO...
+
+echo.
+echo Compilando...
+echo.
+
+.\cbuild.exe archivebuild --script=CBuildFile-StaticLib
+
+rem COPIANDO HEADERS PARA A PASTA INCLUDE DO SOFTWARE EM BUILD
+
+echo.
+echo Copiando headers para pasta includes
+mkdir %APP_DIR%\include
+xcopy src\* %APP_DIR%\include /E /I /Ys
+
+rem COPIANDO STATIC LIBRARY PARA PASTA LIB
+
+echo.
+echo Copiando static library para pasta lib
+mkdir %APP_DIR%\lib
+copy %BUILD_FOLDER%\libcbtest.a %APP_DIR%\lib
+
+rem EMPACOTANDO EM .ZIP
+
+set CURR_DIR=%cd%
+
+cd %BUILD_FOLDER%
+
+echo.
+echo Empacotando... %APP_DIR%.zip
+
+tar acvf %APP_FOLDER%.zip %APP_FOLDER%
+echo Finalizado.
+
+cd %CURR_DIR%
+
+rem BUILD CONCLUIDO
+
+echo.
+echo Build concluido!
