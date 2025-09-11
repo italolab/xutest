a4c5fdedf30a2a0d1c2aebb4d19819f8a4490fba
Italo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2025-09-11 07:42:50 -0300

    docs: Escrevendo a Página de Overview

---
+++ docs/examples/helloworld/main.cpp
@@ -0,0 +1,15 @@
+#include <cbtest/cbtest.h>
+
+TEST_CASE( primeiroTeste, Testes ) {
+    int a = 1;
+    int b = 1;
+    int c = 2;
+    ASSERT_EQUALS( a, b, )
+    ASSERT_TRUE( a == b, )
+    ASSERT_FALSE( a == c, )
+}
+
+int main() {
+    RUN_TEST_CASES_MENU();
+    return 0;
+}
---
+++ docs/js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready( function() {
+    $("#nav").load( "/cbtest/layout/nav.html" );
+    $("#aside").load( "/cbtest/layout/aside.html" );
+    $("#footer").load( "/cbtest/layout/footer.html" );
+    $("#footer").load( "/cbtest/layout/footer.html" );
+
+    $("body").append( "<span id='top-icon'></span>" );
+
+    $("#top-icon").load( "/cbtest/layout/top-icon.html" );
+    
+    $("pre code").each( function( index, element ) {
+        Prism.highlightElement( element );
+    } );
+} );
