5b2016e4e1b161df1d1ee2a520dc8b0ce16a8074
Italo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2026-07-04 08:34:38 -0300

    chore(comp2.sh): Alterado para caminhos relativos

--- test/comp2.sh
+++ test/comp2.sh
@@ -1,4 +1,4 @@
-g++ -I/home/italo/workspace-cpp/xutest/src -o /home/italo/workspace-cpp/xutest/obj/Debug/test/main.o -c main.cpp
+g++ -I../src -o ../obj/Debug/test/main.o -c main.cpp
 
 g++ -o ../bin/Debug/__test ../obj/Debug/test/main.o -L../lib/linux -lxutest -ldl -rdynamic
 
